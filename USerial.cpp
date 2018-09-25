11f82133b938d1c248cf256d04fc3034ff3247cb
João Menezes <jotatri@gmail.com>
2018-09-25 00:16:53 +0000

    mudanças no serial~

--- USerial.cpp
+++ USerial.cpp
@@ -5,12 +5,12 @@ USerial::USerial(){}
 
 void USerial::init(){
   this->device_identificator();
-  while(true){
-    while(!Serial.available());
-    if(Serial.readString() == "*WM: freeing allocated params!"){
-      break;
-    }
-  }
+  // while(true){
+  //   while(!Serial.available());
+  //   if(Serial.readString() == "*WM: freeing allocated params!"){
+  //     break;
+  //   }
+  // }
 }
 
 bool USerial::register_device(){
@@ -45,9 +45,36 @@ bool USerial::register_data(Service s, char* value, int sensitive){
 }
 
 bool USerial::send(const char* DIE, char* send_data){
-  Serial.println(DIE);
-  delay(1000);
-  Serial.println(send_data);
+  bool endpoint_flag = false;
+  while(!endpoint_flag){
+    Serial.println("endpoint");
+    while(!Serial.available());
+    String serial_read = Serial.readString();
+    Serial.println(serial_read);
+    if (serial_read == "OK"){
+      Serial.println(DIE);
+      while(!Serial.available());
+      serial_read = Serial.readString();
+      if (serial_read == DIE)
+        endpoint_flag = true;
+    }
+  }
+  bool data_flag = false;
+  while(!data_flag){
+    Serial.println("data");
+    while(!Serial.available());
+    String serial_read = Serial.readString();
+    if (serial_read == "OK"){
+      Serial.println(send_data);
+      while(!Serial.available());
+      serial_read = Serial.readString();
+      if(serial_read == send_data)
+        data_flag = true;
+    }
+  }
+  // Serial.println(DIE);
+  // Serial.println(send_data);
+  delay(2000);
   while(!Serial.available());
   String p = Serial.readString();
   if( p == "1"){
